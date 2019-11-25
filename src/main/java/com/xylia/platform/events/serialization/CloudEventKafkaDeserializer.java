d04493ef69141dc6082a4aee0369ba98e019d46b
Rajesh Iyer <iyerajesh@gmail.com>
2019-11-25 16:29:27 -0500

    Initial commit

---
+++ src/main/java/com/xylia/platform/events/serialization/CloudEventKafkaDeserializer.java
@@ -0,0 +1,28 @@
+package com.xylia.platform.events.serialization;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.cloudevents.v03.CloudEventImpl;
+import org.apache.kafka.common.serialization.Deserializer;
+
+import java.util.Map;
+
+import static com.xylia.platform.events.util.Json.binaryDecodeValue;
+
+public class CloudEventKafkaDeserializer<T> implements Deserializer<CloudEventImpl> {
+
+    private ObjectMapper mapper;
+
+    @Override
+    public void close() {
+    }
+
+    @Override
+    public void configure(final Map<String, ?> settings, final boolean isKey) {
+        mapper = new ObjectMapper();
+    }
+
+    @Override
+    public CloudEventImpl deserialize(final String topic, final byte[] payload) {
+        return binaryDecodeValue(payload, CloudEventImpl.class);
+    }
+}
